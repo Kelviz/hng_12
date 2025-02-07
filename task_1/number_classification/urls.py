508e1f1d8841bb2ba30d6741d0d6d04f8ac05d3b
Kelviz <mysviurch@gmail.com>
2025-02-07 06:54:44 +0100

    updated urls

--- task_1/number_classification/urls.py
+++ task_1/number_classification/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import NumberClassificationView
 
 urlpatterns = [
-    path('classify-number/', NumberClassificationView.as_view(), name='classify-number'),
+    path('classify-number', NumberClassificationView.as_view(), name='classify-number'),
 ]
